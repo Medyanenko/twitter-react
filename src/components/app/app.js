89aaff72d1af1251e2b62d39265202ef35906f12
medyanenko <t.kudryavska@gmail.com>
2022-01-20 10:04:19 +0200

    add css.module

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -6,6 +6,16 @@ import PostList from "../post-list";
 import PostAddForm from "../post-add-form";
 
 import './app.css';
+import styled from "styled-components";
+
+const AppBlock = styled.div`
+    margin: 0 auto;
+    max-width: 800px;
+`
+
+const StyledAppBlock = styled(AppBlock)`
+    background-color: grey;
+`
 
 
 const App = () => {
@@ -15,7 +25,7 @@ const App = () => {
         {label:"I need a break...", important:false, id:'du'}
     ]
     return (
-        <div className="app">
+        <AppBlock>
             <AppHeader/>
             <div className="search-panel d-flex"> 
                 <SearchPanel/>
@@ -23,7 +33,7 @@ const App = () => {
             </div>
             <PostList posts={data}/>
             <PostAddForm/>
-        </div>
+        </AppBlock>
     )
 }
 
