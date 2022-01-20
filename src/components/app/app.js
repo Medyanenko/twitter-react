c0fa7b228d63e0b5b29b30b015ae40992f0964db
medyanenko <t.kudryavska@gmail.com>
2022-01-20 09:10:22 +0200

    add state for PostListItem

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -7,7 +7,13 @@ import PostAddForm from "../post-add-form";
 
 import './app.css';
 
+
 const App = () => {
+    const data = [
+        {label:"Going to learn React", important:true, id:'sf'},
+        {label:"That is so good", important:false, id:'fh'},
+        {label:"I need a break...", important:false, id:'du'}
+    ]
     return (
         <div className="app">
             <AppHeader/>
@@ -15,7 +21,7 @@ const App = () => {
                 <SearchPanel/>
                 <PostStatusFilter/>
             </div>
-            <PostList/>
+            <PostList posts={data}/>
             <PostAddForm/>
         </div>
     )
